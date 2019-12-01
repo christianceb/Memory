9d7dd03ca07c83409bfdd10044f87b8b0398870f
Christian Ponce <12862119+christianceb@users.noreply.github.com>
2019-12-01 17:22:34 +0800

    Set a strong BASSline such as having Sass and cleaned up App.js

--- Memory/ClientApp/src/App.js
+++ Memory/ClientApp/src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Col, Container, Row } from 'react-bootstrap';
+//import { Route } from 'react-router';
+//import { Layout } from './components/Layout';
+//import { Home } from './components/Home';
+//import { FetchData } from './components/FetchData';
+//import { Counter } from './components/Counter';
 
 export default class App extends Component {
   displayName = App.name
 
   render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
+      <Container fluid>
+        <Row>
+          <Col sm={3}>
+            Contacts Column
+          </Col>
+          <Col sm={9}>
+            Conversation Column
+          </Col>
+        </Row>
+      </Container>
+      //<Layout>
+      //  <Route exact path='/' component={Home} />
+      //  <Route path='/counter' component={Counter} />
+      //  <Route path='/fetchdata' component={FetchData} />
+      //</Layout>
     );
   }
 }
